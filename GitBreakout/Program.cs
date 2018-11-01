3af39dc7d58c236a1939aca7d15d58a951583b1a
Sean Keckler <seankeckler@gmail.com>
2018-11-01 09:25:49 -0400

    Initial Commit

---
+++ GitBreakout/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitBreakout
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+            string input = Console.ReadLine();
+            int num = int.Parse(input);
+        }
+    }
+}
